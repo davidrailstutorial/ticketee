da22210e28facc6c125d1431442db81acd8bfc2f
David Osei <david.osei@meltwater.org>
2012-01-15 20:46:19 +0000

    layout structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'bb2dc040a27c228286684382a6e53ae6a9a6a7de3b6506f2d197da8f219717610cdf401578e4c9473c357e9cfd6c86faabd6e3126389006ca4f138c75c86bf57'
